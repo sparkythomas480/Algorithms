971b6eeb30c599b2ef5b4d360e45bb8bca4f3ddd
Mark Thomas Watkins <mark.thomas480@gmail.com>
2024-10-14 10:48:38 +0200

    Use comparers and add more tests.

--- Algorithms.Tests/InsertionSortTests.cs
+++ Algorithms.Tests/InsertionSortTests.cs
@@ -4,7 +4,9 @@ namespace Algorithms.Tests;
 
 public class InsertionSortTests
 {
+    private static readonly int[] ascendSortedOneElement = new[] { 5 };
     private static readonly int[] ascendSortedFiveElements = new[] { 3, 4, 5, 6, 10 };
+    private static readonly int[] descendSortedOneElement = new[] { 5 };
     private static readonly int[] descendSortedFiveElements = new[] { 10, 6, 5, 4, 3 };
 
     [Fact]
@@ -13,6 +15,22 @@ public class InsertionSortTests
         var exception = Assert.Throws<ArgumentNullException>(() => InsertionSort.DoAscending(null!));
         Assert.Equal("elements", exception.ParamName);
         Assert.Equal("The elements argument must not be null. (Parameter 'elements')", exception.Message);
+    }
+
+    [Fact]
+    public void DoAscending_Works_Correctly_When_Zero_Elements()
+    {
+        var toSort = new List<int>();
+        InsertionSort.DoAscending(toSort);
+        Assert.Empty(toSort);
+    }
+
+    [Fact]
+    public void DoAscending_Works_Correctly_When_One_Element()
+    {
+        var toSort = new List<int> {5};
+        InsertionSort.DoAscending(toSort);
+        Assert.Equal(ascendSortedOneElement, toSort);
     }
 
     [Fact]
@@ -29,6 +47,22 @@ public class InsertionSortTests
         var exception = Assert.Throws<ArgumentNullException>(() => InsertionSort.DoDescending(null!));
         Assert.Equal("elements", exception.ParamName);
         Assert.Equal("The elements argument must not be null. (Parameter 'elements')", exception.Message);
+    }
+
+    [Fact]
+    public void DoDescending_Works_Correctly_When_Zero_Elements()
+    {
+        var toSort = new List<int>();
+        InsertionSort.DoDescending(toSort);
+        Assert.Empty(toSort);
+    }
+
+    [Fact]
+    public void DoDescending_Sorts_Correctly_When_One_Element()
+    {
+        var toSort = new List<int> {5};
+        InsertionSort.DoDescending(toSort);
+        Assert.Equal(descendSortedOneElement, toSort);
     }
 
     [Fact]
--- Algorithms/InsertionSort.cs
+++ Algorithms/InsertionSort.cs
@@ -4,31 +4,39 @@ namespace Algorithms.Sort;
 
 public static class InsertionSort
 {
+    private sealed class DescendingComparer : IComparer<int>
+    {
+        public static readonly DescendingComparer instance = new DescendingComparer();
+        private readonly Comparer<int> ascendingComparer = Comparer<int>.Default;
+
+        int IComparer<int>.Compare(int x, int y)
+        {
+            return ascendingComparer.Compare(y, x);
+        }
+    }
+
     public static void DoAscending(IList<int> elements)
     {
-        Do(elements, (n1, n2) => n1 > n2);
+        Do(elements, Comparer<int>.Default);
     }
 
     public static void DoDescending(IList<int> elements)
     {
-        Do(elements, (n1, n2) => n1 < n2);
+        Do(elements, DescendingComparer.instance);
     }
 
-   private static void Do(IList<int> elements, Func<int, int, bool> comparer)
+   private static void Do(IList<int> elements, IComparer<int> comparer)
    {
         Debug.Assert(comparer != null);
 
          if (elements == null)
             throw new ArgumentNullException(nameof(elements), "The elements argument must not be null.");
 
-        if (elements.Count == 0 || elements.Count == 1)
-            return;
-
         for (int i = 1; i < elements.Count; ++i)
         {
             int key = elements[i];
             int j = i - 1;
-            while (j >= 0 && comparer(elements[j], key))
+            while (j >= 0 && comparer.Compare(elements[j], key) > 0)
             {
                 elements[j + 1] = elements[j];
                 j -= 1;
